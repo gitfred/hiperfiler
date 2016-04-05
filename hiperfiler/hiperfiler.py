d5b77be4c56a21222084a78e1a6b7c6af0df4f5d
Sylwester Brzeczkowski <sbrzeczkowski@mirantis.com>
2016-04-05 11:19:44 +0200

    initial

---
+++ hiperfiler/hiperfiler.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/files")
+def get_files_list():
+    files = os.listdir(os.path.expanduser('~'))
+    return flask.jsonify({"files": files})
+
+
+@app.route("/files/<name>")
+def get_file_metadata(name):
+    files = os.path.expanduser('~')
+    path = os.path.join(files, name)
+    data = {
+        "path": path,
+        "size": os.path.getsize(path),
+        "mtime": os.path.getmtime(path),
+        "ctime": os.path.getctime(path),
+    }
+
+    return flask.jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
